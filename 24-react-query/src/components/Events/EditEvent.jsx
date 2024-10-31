d19c4e731c267e968f9c0879780f5db0641d95ee
selmasaltik <selmasaltiik@gmail.com>
2024-11-01 02:21:05 +0300

    Updating Data with Mutations

--- 24-react-query/src/components/Events/EditEvent.jsx
+++ 24-react-query/src/components/Events/EditEvent.jsx
@@ -1,20 +1,64 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { fetchEvent, updateEvent } from '../../util/http.js';
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
+  const id = params.id;
 
-  function handleSubmit(formData) {}
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id })
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: updateEvent
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id, event: formData });
+    navigate('../');
+  }
 
   function handleClose() {
     navigate('../');
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  let content;
+
+  if(isPending) {
+    content = (
+      <div className="center">
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if(isError) {
+    content = (
+      <>
+        <ErrorBlock 
+          title="Failed to load event" 
+          message={error.info?.message || 'Failed to load event. Please check your inputs and try again later.'} 
+        />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link> 
+        </div>
+      </>
+    );
+  }
+
+  if(data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +66,8 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
-    </Modal>
-  );
+    );
+  }
+
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
